879bc0c28ea46c46f7c13c73ea05ea6e193c77b9
Ahmed <mshah1@cmu.edu>
2016-05-16 14:55:50 +0300

    buff size

--- server_test.py
+++ server_test.py
@@ -14,7 +14,7 @@ while True:
     [headers,body] = data.split('\n\n')
     [filename, size] = headers.split('\n')
     size = int(size)
-    body += c.recv(size)
+    body += c.recvall(size - len(body))
     print data
     print "size recv = " + str(len(body))
     f = open("files/"+filename,'w')
