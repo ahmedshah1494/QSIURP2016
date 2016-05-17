d862c5b867ca12c3b5e67300e2687d3c390db01b
Muhammad Ahmed Shah <mshah1@makoshark.ics.cs.cmu.edu>
2016-05-17 04:18:28 -0400

    with java server

---
+++ SpeechServer.java
@@ -0,0 +1,40 @@
+
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class SpeechServer {
+	private final static String path = "files/";
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try {
+			ServerSocket ss = new ServerSocket(9999);
+			while (true){
+				Socket cs = ss.accept();
+				System.out.println(cs.getInetAddress().getHostAddress());
+				ObjectInputStream in = new ObjectInputStream(cs.getInputStream());
+				String data = (String) in.readObject();
+				String[] split = data.split("\n\n");
+				String headers = split[0];
+				String body = split[1];
+				String filename = headers.split("\n")[0];
+				int size = Integer.parseInt(headers.split("\n")[1]);
+				
+				FileWriter f = new FileWriter(path + filename);
+				f.write(body);
+				f.close();
+				
+				cs.close();
+			}
+		} catch (IOException | ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+}
