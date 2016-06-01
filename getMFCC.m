7ceb909dff1e5d0eec30dd5d35cc1387f082f9db
Muhammad Ahmed Shah <mshah1@qatar.win.cmu.edu>
2016-06-02 01:09:00 +0300

    get mfcc

--- getMFCC.m
+++ getMFCC.m
@@ -1,15 +1,20 @@
-files_d = '\\andrew.ad.cmu.edu\users\users8\mshah1\Desktop\QSIURP2016\files\1030\';
-files = dir(files_d);
-for i = 3:numel(files):
-    s
-for i = 3:numel(files)
-    fname = files(i).name;
-    display(fname);
-    if (fname(length(fname)-3:length(fname)) == '.wav')
-        display(strcat(d,fname));
-        [y,Fs] = audioread(strcat(d,fname));
-        [CCs,FBEs,frames] = mfcc(y,Fs,25,10,0.97,hamming(44100),[50 15000],20,13,22);
-        dlmwrite(strcat(d,fname,'.mfcc'),transpose(CCs));
-        dlmwrite(strcat(d,fname,'.fbe'),transpose(FBEs));
+files_d = '\\qatar.win.cmu.edu\files\User\mshah1\windowsoverhead\Desktop\QSIURP2016\files\';
+fldrs = dir(files_d);
+for j = 3:numel(fldrs)
+    if isdir(strcat(files_d,fldrs(j).name))      
+        files = dir(strcat(files_d,fldrs(j).name));
+        for i = 3:numel(files)
+            fname = files(i).name;
+            display(fname);
+            if (fname(length(fname)-3:length(fname)) == '.wav')
+                fpath = (strcat(files_d,fldrs(j).name,'\',fname));
+                display(fpath)
+                [y,Fs] = audioread(fpath);
+                hamming = @(N)(0.54-0.46*cos(2*pi*[0:N-1].'/(N-1)));
+                [CCs,FBEs,frames] = mfcc(y,Fs,25,10,0.97,hamming,[50 15000],20,13,22);
+                dlmwrite(strcat(fpath,'.mfcc'),transpose(CCs));
+                dlmwrite(strcat(fpath,'.fbe'),transpose(FBEs));
+            end
+        end
     end
-ends
+end
