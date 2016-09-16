b0b2a641e12124b1a5b8243ded67379425a45cbf
Ahmed Shah <Ahmed Shah>
2016-09-16 04:59:41 -0400

    pushing remaining files

--- code_anurag/analyseResults.py
+++ code_anurag/analyseResults.py
@@ -81,51 +81,87 @@ def getAccuracyOverTime(roomType,nComps,nFolds,interval,resultsFile, svm=False):
 	for c in sorted(probs_PByTime):
 		resultsFile.write("%s %d 		%s\n" % (roomType, c, str(probs_PByTime[c])))
 
-def makeDETCurve(PosResultsFile, NegResultsFile, duration):
-	f = open(PosResultsFile, 'r')
-	lines_P = f.readlines()
-	f.close()
+def makeDETCurve(resFolder, duration, divs):
+	finalFA = np.array([0.0]*divs)
+	finalMD = np.array([0.0]*divs)
+	for i in range(4):
+		f = open(resFolder+"/fold_%d/64/P.txt" % i, 'r')
+		lines_P = f.readlines()
+		f.close()
 
-	f = open(NegResultsFile, 'r')
-	lines_N = f.readlines()
-	f.close()
+		f = open(resFolder+"/fold_%d/64/N.txt" % i, 'r')
+		lines_N = f.readlines()
+		f.close()
 
-	nlines = duration/3
-	lines_P = lines_P[:nlines]
-	lines_P = map(lambda x: map(float,x.split()[1:]), lines_P)
-	probs_P = map(lambda x: 100000*(x[0] / sum(x)), lines_P)
-	
-	lines_N = lines_N[:nlines]
-	lines_N = map(lambda x: map(float,x.split()[1:]), lines_N)
-	probs_N = map(lambda x: 100000*(x[0] / sum(x)), lines_N)
+		nlines = duration/3
+		lines_P = lines_P[:nlines]
+		lines_P = map(lambda x: map(float,x.split()[1:]), lines_P)
+		probs_P = map(lambda x: 100000*(x[0] / sum(x)), lines_P)
+		
+		lines_N = lines_N[:nlines]
+		lines_N = map(lambda x: map(float,x.split()[1:]), lines_N)
+		probs_N = map(lambda x: 100000*(x[0] / sum(x)), lines_N)
 
-	allProbs = probs_P + probs_N
-	thresh = range(int(min(allProbs)), int(max(allProbs)), int(max(allProbs) - min(allProbs))/10) 
-	
-	MD = []
-	FA = []
-	for th in thresh:
-		labels = []
-		for p in probs_P:
-			if p > th:
-				labels.append(0)
-			else:
-				labels.append(1)
-		MD.append(sum(labels)/float(len(labels)))
+		allProbs = probs_P + probs_N
+		thresh = range(int(min(allProbs)), int(max(allProbs)), (int(max(allProbs) - min(allProbs))/divs)) 
+		
+		MD = []
+		FA = []
+		for th in thresh:
+			labels = []
+			for p in probs_P:
+				if p > th:
+					labels.append(0)
+				else:
+					labels.append(1)
+			MD.append(sum(labels)/float(len(labels)))
 
-		labels = []
-		for p in probs_N:
-			if p > th:
-				labels.append(1)
-			else:
-				labels.append(0)
-		FA.append(sum(labels)/float(len(labels)))
+			labels = []
+			for p in probs_N:
+				if p > th:
+					labels.append(1)
+				else:
+					labels.append(0)
+			FA.append(sum(labels)/float(len(labels)))
+		MD = np.array(MD[:divs])
+		FA = np.array(FA[:divs])
+
+		finalMD += MD
+		finalFA += FA
 
 		# print th, labels
-	print MD
-	print FA
+	finalMD = map(lambda x: x/4, finalMD)
+	finalFA = map(lambda x: x/4, finalFA)
 
-makeDETCurve("results/withSannan_64ms/testOnSingleRoom/BR/fold_2/64/P.txt", "results/withSannan_64ms/testOnSingleRoom/BR/fold_2/64/N.txt", 100)
+	eqLinePts = map(lambda x: x/float(divs), range(divs))
+	myPoint = map(lambda x: [finalFA[x], finalMD[x]], range(divs))
+	diffs = map(lambda x : np.sqrt((myPoint[x][0] - eqLinePts[x])**2 - (myPoint[x][1] - eqLinePts[x])**2), range(divs))
+	# print eqLinePts	
+	# diffs = map(lambda x: (finalFA[x] - x)**2 + (finalMD[x] - eqLinePts[x]))
+	# # print finalMD
+	diffs = map(abs, np.array(finalMD) - np.array(finalFA))
+	print diffs
+	minDiffIdx = np.argmin(diffs)
+	# minDiffIdx = np.argwhere(np.isclose(finalMD, eqLinePts)).reshape(-1)
+	print minDiffIdx
+	print resFolder, finalMD[minDiffIdx]
+
+	thresh = map(lambda x: x/100000.0, thresh)
+	plt.plot(finalFA,finalMD)
+	# print finalFA
+	# print finalMD
+	
+	
+for c in ["BR", "BH", "SH", "O", "P"]:
+	makeDETCurve("results/withSannan_64ms/testOnSingleRoom/"+c, 600, 300)
+eqLinePts = map(lambda x: x/1000.0, range(1000))
+plt.plot(eqLinePts,eqLinePts)
+plt.ylabel("False Negative %")
+plt.xlabel("False Positive %")
+plt.legend(['Bathrooms', "Big Lecture Halls", 'Classrooms', 'Offices', 'Pantries'], loc=0)
+	# plt.plot(finalFA,thresh)
+# plt.show()
+plt.savefig('results/withSannan_64ms/testOnSingleRoom/analysis/DETcurve.png')
 # f = open('results/%s/analysis/results.txt' % (subfolder),'w')
 # for c in ["BR", "BH", "SH", "O", "P"]:
 # 	getAccuracyOverTime(c, 64, 4,30,f,svm=True)
--- code_anurag/getSVMParams.sh
+++ code_anurag/getSVMParams.sh
@@ -9,8 +9,8 @@ for f in $folds
 do
 	for c in ${classes[@]}
 	do
-		qsub -q hp -N gridSearch.$f.$c.txt -e $errd/err.gridSearch.$f.$c.txt -o out.gridSearch.$f.$c.txt -v fld=$f,cls=$c,logf=$logd/log.gridSearch.$f.$c.txt grid.sh
-		#echo SVMTrainVectors_$c"_fld$f".txt 
-		python ../../libsvm-3.21/tools/grid.py ../files/folds/temp/$c/SVMTrainVectors_$c"_fld"$f.txt
+		#qsub -q hp -N gridSearch.$f.$c.txt -e $errd/err.gridSearch.$f.$c.txt -o out.gridSearch.$f.$c.txt -v fld=$f,cls=$c,logf=$logd/log.gridSearch.$f.$c.txt grid.sh
+		echo SVMTrainVectors_$c"_fld$f".txt 
+		python ../libsvm-3.21/tools/grid.py ../files/folds/temp/$c/SVMTrainVectors_$c"_fld"$f.txt > SVMTrainVectors_$c"_fld"$f.grid.txt
 	done
 done
